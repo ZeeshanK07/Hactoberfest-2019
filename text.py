f541df2bcc3da28204bd312abeb740cb5baa9084
Zeeshan <66550180+ZeeshanK07@users.noreply.github.com>
2021-10-22 22:36:31 +0530

    Update text.py
    Integrated user input with the loops so that the user can choose how much a user wants to print the number

--- text.py
+++ text.py
@@ -1,3 +1,8 @@
-print (this pr is made for educational purposes)
-for i in range(0,5)
- print (i)
+print ("this pr is made for educational purposes")
+
+var=int(input("Enter the number which till you want to print: "))
+
+i=0    
+while (i<=var):
+    print(i)
+    i=i+1
